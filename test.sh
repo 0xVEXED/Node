7cd7e54d7a48f7a8cd79ae248a3282304cab8400
VΣXED <139860198+0xVEXED@users.noreply.github.com>
2025-03-29 14:12:54 +0300

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+# Цвета текста
+RED='\033[0;31m'
+BLUE='\033[0;34m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+# лого
+curl -s https://raw.githubusercontent.com/0xVEXED/Node/main/logo.sh | bash
+
+# Меню
+echo -e "${RED}Выберите действие:${NC}"
+echo -e "${GREEN}1) Установка ноды${NC}"
+echo -e "${GREEN}2) Обновление ноды${NC}"
+echo -e "${GREEN}3) Удаление ноды${NC}"
+
+echo -e "${RED}Введите номер:${NC} "
+read choice
